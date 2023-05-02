8d3f14aef5475e2924a5b32a1a1a7961adeda99f
Carla Blacio Reyes <carly.isa.0812@gmail.com>
2023-05-02 15:38:51 -0600

    tarea9InterfazGrafica1

---
+++ tarea9InterfazGrafica/src/main/java/ICO/FES/Main.java
@@ -0,0 +1,9 @@
+package ICO.FES;
+
+import ICO.FES.interfazUsuario.Principal;
+
+public class Main {
+    public static void main(String[] args){
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
---
+++ tarea9InterfazGrafica/src/main/java/ICO/FES/interfazUsuario/eventos/eventosMouse.java
@@ -0,0 +1,24 @@
+package ICO.FES.interfazUsuario.eventos;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class eventosMouse implements MouseListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Se dio click!");
+    }
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+}//end
