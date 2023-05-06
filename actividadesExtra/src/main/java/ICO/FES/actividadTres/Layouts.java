34f5f8097f5f460f8c26dfc40cf45210851d8f6e
Carla Blacio Reyes <carly.isa.0812@gmail.com>
2023-05-06 13:00:44 -0600

    actividaTresLayouts

---
+++ actividadesExtra/src/main/java/ICO/FES/actividadTres/Layouts.java
@@ -0,0 +1,8 @@
+package ICO.FES.actividadTres;
+
+public class Layouts {
+    public static void main(String[] args) {
+        //Ventana ventana = new Ventana("Layouts");
+        VentanaGrind ventanaGrind= new VentanaGrind("Ventana GridLayouts cuadricula");
+    }
+}
