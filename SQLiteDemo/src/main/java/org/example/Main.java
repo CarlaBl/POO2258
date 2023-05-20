d3ae65ed4f20fd252da9c17764016f79020c9228
Carla Blacio Reyes <carly.isa.0812@gmail.com>
2023-05-20 17:44:48 -0600

    Singleton y PreparedStatement

--- SQLiteDemo/src/main/java/org/example/Main.java
+++ SQLiteDemo/src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import org.example.Modelo.Libro;
+import org.example.Persistencia.DemoLibroDB;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Con Statement");
+        /* demo.insertarStatement();
+        System.out.println("Con Prepared");
+        demo.insertarPreparedStatement(); **/
+
+        DemoLibroDB demo = new DemoLibroDB();
+        Libro libro = new Libro(0,"El juego","desconocido");
+
+        /* if(demo.insertarlibro(libro)){
+            System.out.println("Se inserto correctamente");
+        }else {
+            System.out.println("No se inserto");
+        } **/
+
+        System.out.println(demo.buscarLibroPorId(100));
+        System.out.println("----------------");
+        for (Libro tmp: demo.obtenerTodos()) {
+            System.out.println("Libro: " + tmp);
+            System.out.println("Titulo: " + tmp.getTitulo());
+
+        }
+
     }
 }
